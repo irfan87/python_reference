5c9cf3f97f262b3c6d567d205c6adb1787055290
irfan87 <nerve2009@yahoo.com>
2019-09-29 02:05:13 +0800

    a simple hello world application

---
+++ chapter_1/hello_world.py
@@ -0,0 +1,6 @@
+# TODO: define a hard-coded variable 
+my_first_name = "ahmad irfan"
+my_last_name = "mohammad shukri"
+my_fullname = my_first_name + " " + my_last_name
+
+print(f"hey, {my_fullname.title()}")
